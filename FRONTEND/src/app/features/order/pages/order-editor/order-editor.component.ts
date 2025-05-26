65141208e7003d7848ebce0f0cfa2cc0d901787c
fenyvesi.barnabas55@stud.uni-obuda.hu <fenyvesi.barnabas55@stud.uni-obuda.hu>
2025-05-27 00:41:29 +0200

    orderupdate done

--- FRONTEND/src/app/features/order/pages/order-editor/order-editor.component.ts
+++ FRONTEND/src/app/features/order/pages/order-editor/order-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OrderService } from '../../services/order.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-editor',
@@ -15,8 +15,10 @@ export class OrderEditorComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private orderService: OrderService,
-    private router: Router
-  ) {
+    private router: Router,
+    private route: ActivatedRoute
+  ) 
+  {
     this.form = this.fb.group({
       id:['',Validators.required],
       orderName: ['', Validators.required],
@@ -27,14 +29,34 @@ export class OrderEditorComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  orderIdToEdit: string | null = null;
 
-  onSubmit(): void {
-    if (this.form.valid) {
-      this.orderService.createOrder(this.form.value).subscribe({
-        next: () => this.router.navigate(['/orders']),
-        error: (err) => console.error('Rendelés mentési hiba:', err)
+
+  ngOnInit(): void {
+  this.route.paramMap.subscribe(params => {
+    const id = params.get('id');
+    if (id) {
+      this.orderIdToEdit = id;
+      this.orderService.getOrderById(id).subscribe(order => {
+        this.form.patchValue(order);
+      });
+    }
+  });
+}
+
+  onSubmit() {
+  if (this.form.valid) {
+    if (this.orderIdToEdit) {
+      // Szerkesztés
+      this.orderService.updateOrder(this.orderIdToEdit, this.form.value).subscribe(() => {
+        this.router.navigate(['/orders']);
+      });
+    } else {
+      // Új rendelés
+      this.orderService.createOrder(this.form.value).subscribe(() => {
+        this.router.navigate(['/orders']);
       });
     }
   }
+}
 }
